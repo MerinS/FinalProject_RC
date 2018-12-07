eb2fe226dbad80d749d780de48a1686bda266a26
MerinS <myselfmerin@gmail.com>
2018-12-07 00:18:48 -0500

    Update reg.vhd

--- reg.vhd
+++ reg.vhd
@@ -22,27 +22,26 @@ use IEEE.STD_LOGIC_1164.all;
 -- output: output from the register
 -------------------------------------------------------------------------------
 
-entity REG is
-  generic (width : positive  := 32;
-           init  : std_logic := '0');
-  port(clk    : in  std_logic;
-       rst    : in  std_logic;
-       en     : in  std_logic;
-       input  : in  std_logic_vector(width-1 downto 0);
-       output : out std_logic_vector(width-1 downto 0));
-end REG;
+entity reg is
+generic (width : positive := 16);
+port(clk       : in  std_logic;
+   rst       : in  std_logic;
+   en        : in  std_logic;
+   input     : in  std_logic_vector(width-1 downto 0);
+   output    : out std_logic_vector(width-1 downto 0));
+end reg;
 
-architecture bhv of REG is
+architecture Behavioral of reg is
+
 begin
   process(clk, rst)
   begin
-    if rst = '1' then
-      output <= (others => 0);
-    elsif (clk = '1' and clk'event) then
+    if (rst = '1') then
+      output   <= (others => '0');
+    elsif (clk'event and clk = '1') then
       if (en = '1') then
         output <= input;
       end if;
     end if;
   end process;
-end bhv;
-
+end Behavioral;
